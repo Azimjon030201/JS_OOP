5dd337f562d28639510dce0f3e41c8655a718b17
Azimjon030201 <hazard030201@gmail.com>
2025-11-06 15:17:49 +0500

    Lesson9 Constructors (under the hood)

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-class User {
-    constructor(name, email){
-        this.name = name;
-        this.email = email
-        this.score = 0;
-    }
-    login(){
+function User(name, email){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
         console.log(this.email, "is just logged in");
-        return this
-    }
-    logout(){
-        console.log(this.email, "is just logged out")
-        return this
-    }
-    updateScore(){
-        this.score++
-        console.log(this.name , "score is now" , this.score);
-        return this
-    }
-}
-
-
-class Admin extends User{
-    deleteUser(user){
-        users = users.filter(u=>{
-            return u.email !=user.email
-        })
+        
     }
 }
 var userOne = new User("Azimjon", "hazard030201@gmail.com")
 var userTwo = new User("Hello2", "hello2@gmail.com")
-userOne.login().updateScore().updateScore().logout()
 
-var admin = new Admin("hello2","hello2@gmail.com")
-var users = [userOne, userTwo]
-admin.deleteUser(userTwo);
 
-console.log(users);
