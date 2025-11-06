61f7aa0d1d9c900e4d48af25003d3d83b45ebd66
Azimjon030201 <hazard030201@gmail.com>
2025-11-06 15:02:44 +0500

    Lesson 8 Inheritence

--- index.js
+++ index.js
@@ -18,8 +18,21 @@ class User {
         return this
     }
 }
+
+
+class Admin extends User{
+    deleteUser(user){
+        users = users.filter(u=>{
+            return u.email !=user.email
+        })
+    }
+}
 var userOne = new User("Azimjon", "hazard030201@gmail.com")
 var userTwo = new User("Hello2", "hello2@gmail.com")
 userOne.login().updateScore().updateScore().logout()
 
-console.log(userOne);
+var admin = new Admin("hello2","hello2@gmail.com")
+var users = [userOne, userTwo]
+admin.deleteUser(userTwo);
+
+console.log(users);
