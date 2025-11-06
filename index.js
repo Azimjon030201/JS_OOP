a5a1ad4afd5fb31d7a59a6ce47baa0436f802e87
Azimjon030201 <hazard030201@gmail.com>
2025-11-06 14:32:46 +0500

    Lesson 6

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ class User {
         this.name = name;
         this.email = email
     }
+    login(){
+        console.log(this.email, "is just logged in");
+    }
+    logout(){
+        console.log(this.email, "is just logged out")
+    }
 }
 var userOne = new User("Azimjon", "hazard030201@gmail.com")
 var userTwo = new User("Hello2", "hello2@gmail.com")
