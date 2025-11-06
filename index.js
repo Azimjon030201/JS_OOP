ad4898029519b8970a2ff75340bd1632f649f788
Azimjon030201 <hazard030201@gmail.com>
2025-11-06 12:56:21 +0500

    Lesson 2 davomi

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-var userOneEmail = "hello1@gmail.com"
-var userOneName = "hello1"
-var userOneFriends = ["hello2"]
-
-var userTwoEmail = "hello2@gmail.com"
-var userTwoName = "hello2"
-var userTwoFriends = ["hello3"]
-
-var userThreeEmail = "hello3@gmail.com"
-var userThreeName = "hello3"
-var userThreeFriends = ["hello1"]
-
-function login(email){
-    console.log(email, "is now online");
-    
+var userOne = {
+    name: "hello1",
+    email: "hello1@gmail.com",
+    login(){
+        console.log(this.email, "has logged in");
+    },
+    logout(){
+        console.log(this.email, "has logged out");
+    }
 }
-function logout(email){
-    console.log(email, "is now offline");
-    
-}
-function logFriends(friends){
-    friends.forEach(friend => {
-        console.log(friend);
-    });
-}
-
-login(userOneEmail);
