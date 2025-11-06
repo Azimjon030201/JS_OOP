23acdfaa11939b9ce52474fd97a62d6655078964
Azimjon030201 <hazard030201@gmail.com>
2025-11-06 15:29:14 +0500

    Lesson 10 Prototype

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ function User(name, email){
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function(){
-        console.log(this.email, "is just logged in");
-        
-    }
+
+}
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, "is just logged in");
+}
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, "is just logged in");
 }
 var userOne = new User("Azimjon", "hazard030201@gmail.com")
 var userTwo = new User("Hello2", "hello2@gmail.com")
