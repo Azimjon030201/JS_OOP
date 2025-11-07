ff567e5bb12b3e1f47a36e8ef5bf38ffab99eb14
Azimjon030201 <hazard030201@gmail.com>
2025-11-07 10:35:53 +0500

    Lesson 11 Prototype inheritence

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ User.prototype.logout = function(){
     this.online = false;
     console.log(this.email, "is just logged in");
 }
+
+function Admin(...args){
+    User.apply(this, args);
+    this.role = "super admin";
+
+}
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user =>{
+        return user.email != u.email;
+    })
+}
 var userOne = new User("Azimjon", "hazard030201@gmail.com")
 var userTwo = new User("Hello2", "hello2@gmail.com")
+var admin = new Admin("hello3", "hello3@gmail.com")
 
+var users = [userOne, userTwo, admin]
 
+admin.deleteUser(userTwo);
